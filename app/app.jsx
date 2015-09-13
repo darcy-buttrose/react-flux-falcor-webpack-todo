b5d9502387d824f14a2dc58a200795aa58b92367
darcy-buttrose <darcy.buttrose@gmail.com>
2015-09-13 10:58:17 +0800

    init

---
+++ app/app.jsx
@@ -0,0 +1,10 @@
+/**
+ * Created by Darcy on 13/09/2015.
+ */
+import React from "react";
+
+export default class App extends React.Component {
+    render() {
+        return <h1>hello world!</h1>;
+    }
+}
---
+++ app/entry.jsx
@@ -0,0 +1,10 @@
+/**
+ * Created by Darcy on 13/09/2015.
+ */
+import App from "./app";
+import React from "react";
+
+React.render(
+    <App/>,
+    document.getElementById("content")
+);
